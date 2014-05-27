a6055e66012223f3a68f5c087f622bbca8c675cc
Rav Tonsiengsom <elrohin@gmail.com>
2014-05-27 17:09:42 -0500

    Support for HAL representation of HATEOAS

--- labs/java-spring-boot/src/main/java/com/nvisia/restlab/Application.java
+++ labs/java-spring-boot/src/main/java/com/nvisia/restlab/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @Configuration
 @EnableAutoConfiguration
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 @ComponentScan
 public class Application {
 
